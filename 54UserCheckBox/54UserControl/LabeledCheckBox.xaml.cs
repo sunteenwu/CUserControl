c8efe5c7de37cac1b33d7420a8db2397d1dcbabe
sunteenwu <wu_15xun@163.com>
2017-05-04 11:09:50 +0800

    Add the test project

---
+++ 54UserCheckBox/54UserControl/LabeledCheckBox.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace _54UserControl
+{
+    public sealed partial class LabeledCheckBox : UserControl
+    {
+        public LabeledCheckBox()
+        {
+            this.InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(LabeledCheckBox), null);
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register("IsChecked", typeof(bool), typeof(LabeledCheckBox), null);
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        public static readonly new DependencyProperty StyleProperty = DependencyProperty.Register("Style", typeof(Style), typeof(LabeledCheckBox), null);
+
+        public new Style Style
+        {
+            get { return (Style)GetValue(StyleProperty); }
+            set { SetValue(StyleProperty, value); }
+        }
+    }
+
+}
